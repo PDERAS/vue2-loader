277fa2c23648e9f270d92bdc95aee9bf39f03ed9
Cody Moorhouse <cody.moorhouse@pderas.com>
2018-03-14 12:10:38 -0600

    Change export of isntall function (I.E. Fix)

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import Loader from './components/Loader.vue'
 import options from './options';
 
-export default {
-    install (Vue, globalOptions) {
-        if (globalOptions) {
-            Object.keys(globalOptions).map(function(key) {
-                options[key] = globalOptions[key];
-            });
-        }
-        Vue.component('loader', Loader);
+function install (Vue, globalOptions) {
+    if (globalOptions) {
+        Object.keys(globalOptions).map(function(key) {
+            options[key] = globalOptions[key];
+        });
     }
+    Vue.component('loader', Loader);
 }
+
+export default install;
